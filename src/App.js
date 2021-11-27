4bbbf28a9763143bcda6f7e755c286b3072baa2f
John Covenant <ufotjohnamos@gmail.com>
2021-11-27 20:11:05 +0100

    update

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App() {
     {
       id: "e3",
       title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
+      amount: 294.68,
+      date: new Date(2021, 3, 28),
     },
     {
       id: "e4",
