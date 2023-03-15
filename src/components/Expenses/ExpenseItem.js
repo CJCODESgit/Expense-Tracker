a2234d6e10470cc39377f84586d6bf7d60bd8a4f
johncovenant <ufotjohnamos@gmail.com>
2023-03-15 14:31:24 +0100

    update functions to arrow functions

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,7 +3,7 @@ import ExpenseDate from "./ExpenseDate";
 import Card from '../UI/Card';
 import "./ExpenseItem.css";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
  
   return (
     <Card className="expense-item">
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,7 +3,7 @@ import './Expenses.css';
 import Card from '../UI/Card';
 import ExpenseItem from './ExpenseItem';
 
-function Expenses(props) {
+const Expenses = (props) => {
     return (
     <Card className='expenses'>
       <ExpenseItem
