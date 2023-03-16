b84698dd945f5576959bfb16241f53a7a2140599
johncovenant <ufotjohnamos@gmail.com>
2023-03-16 12:58:51 +0100

    new

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -12,6 +12,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">₦{props.amount}</div>
       </div>
+      <button>Change Title</button>
     </Card>
   ); 
 }
