142b46afd5e32cd6dae4cf4e243ff11d4d1694b2
johncovenant <ufotjohnamos@gmail.com>
2023-03-20 12:20:27 +0100

    setTitle

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,14 @@
-import React, {  } from 'react';
+import React, { useState } from 'react';
 import ExpenseDate from "./ExpenseDate";
 import Card from '../UI/Card';
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
 
-  let title = props.title;
+  const [title, setTitle] = useState(props.title);
 
   const clickHandler = () => {
-    title = 'Updated!';
+    setTitle('updated!');
     console.log(title);
   };
  
