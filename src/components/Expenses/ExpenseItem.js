f35b88d3fb09810c459a1e85dd3dfddcfa57bf5f
johncovenant <ufotjohnamos@gmail.com>
2023-03-20 23:55:11 +0100

    rand

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -18,7 +18,7 @@ const ExpenseItem = (props) => {
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">₦{props.amount}</div>
-      </div>
+      </div> 
       <button onClick={clickHandler}>Change Title</button>
     </Card>
   ); 
