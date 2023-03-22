4d2c49442ccf0d17c6e279fc8178aae07dadf5ca
johncovenant <ufotjohnamos@gmail.com>
2023-03-22 15:29:56 +0100

    split form comp

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './ExpenseForm.css';
+
+const ExpenseForm = () => {
+return 
+<form>
+    
+</form>
+}
+
+export default ExpenseForm;
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -4,7 +4,7 @@ import './NewExpense.css';
 
 const NewExpense = () => {
     return <div className='new-expense'>
-        <form></form>
+        
     </div>
 }
 
