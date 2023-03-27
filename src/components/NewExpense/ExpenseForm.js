ce5c026017f3f80b7ffbc8e6c5d4c5b3c2fc1333
johncovenant <ufotjohnamos@gmail.com>
2023-03-27 13:21:49 +0100

    n

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,11 +2,11 @@ import React, {useState} from "react";
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-    // const [enteredTitle, setEnteredTitle] = useState('');
-    // const [enteredAmount, setEnteredAmount] = useState('');
-    // const [enteredDate, setEnteredDate] = useState('');
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
 
-    const [userInput, setUSerInp ]
+    // const [userInput, setUSerInp ]
 
     const titleChangeHandler = (event) => {
       setEnteredTitle(event.target.value);
