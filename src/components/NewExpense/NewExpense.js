4a985d6072354c52cad61efca93b9b8611c34de2
johncovenant <ufotjohnamos@gmail.com>
2023-03-29 14:06:08 +0100

    new expense form fix

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -4,9 +4,11 @@ import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = () => {
-    return <div className='new-expense'>
-        <ExpenseForm />
+    return (
+    <div className='new-expense'>
+        <ExpenseForm onSaveExpenseData />
     </div>
-}
+    )
+};
 
 export default NewExpense;
