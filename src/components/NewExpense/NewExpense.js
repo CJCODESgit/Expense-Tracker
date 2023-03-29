e375abc7b844f8d47f8871e1d3f7aeca1475de83
johncovenant <ufotjohnamos@gmail.com>
2023-03-29 14:15:24 +0100

    SavExpenseDataHandler Start

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -4,6 +4,12 @@ import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = () => {
+    const onSaveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData
+        };
+    };
+
     return (
     <div className='new-expense'>
         <ExpenseForm onSaveExpenseData />
