e5850154b3213199b40ed4e323457daead4f40c7
johncovenant <ufotjohnamos@gmail.com>
2023-04-04 15:28:38 +0100

    refactored

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -23,31 +23,7 @@ const Expenses = (props) => {
     />
     ))}
 
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-       />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-       /> 
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-       />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-       />
-        <ExpenseItem
-        title={props.items[4].title}
-        amount={props.items[4].amount}
-        date={props.items[4].date}
-       />
+      
     </Card>
     </div>
     )
