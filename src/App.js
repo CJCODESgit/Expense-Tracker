7db470eabc3e0052c2c5dbed390186abbb85c87a
johncovenant <ufotjohnamos@gmail.com>
2023-04-12 00:15:04 +0100

    app

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import "./App.css";
 import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from "./components/Expenses/Expenses";
 
 const DUMMY_EXPENSES = [
-    {
-      id: "e1", 
-      title:  "Starlink subscription",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    {
-      id: "e2",
-      title: "CPU Build",
-      amount: 799.49,
-      date: new Date(2019, 2, 12),
-     },
-    {
-      id: "e3",
-      title: "FHD Monitor",
-      amount: 294.68,
-      date: new Date(2021, 3, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk",
-      amount: 450,
-      date: new Date(2022, 4, 6),
-    },
-     
-  ];
+  {
+    id: "e1",
+    title: "Starlink subscription",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: "e2",
+    title: "CPU Build",
+    amount: 799.49,
+    date: new Date(2019, 2, 12),
+  },
+  {
+    id: "e3",
+    title: "FHD Monitor",
+    amount: 294.68,
+    date: new Date(2021, 3, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk",
+    amount: 450,
+    date: new Date(2022, 5, 6),
+  },
+
+];
 
 const App = () => {
-  const [expenses, setExpenses] =  useState(DUMMY_EXPENSES);
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
 
   const addExpenseHandler = expense => {
@@ -44,7 +44,7 @@ const App = () => {
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items = {expenses} />
+      <Expenses items={expenses} />
     </div>
   );
 }
