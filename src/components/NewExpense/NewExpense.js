feeac809770d57fae927ab0d3e5ce6a2e0f857f4
johncovenant <ufotjohnamos@gmail.com>
2023-04-11 11:46:49 +0100

    conditional form cancel button

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -11,16 +11,21 @@ const NewExpense = (props) => {
             id: Math.random().toString()
         };
         props.onAddExpense(expenseData);
+        setIsEditing(false)
     };
 
     const startEditingHandler = () => {
         setIsEditing(true)
     };
 
+    const stopEditingHandler = () => {
+        setIsEditing(false)
+    }
+
     return (
     <div className='new-expense'>
         {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
-        {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />}
+        {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler} />}
     </div>
     );
 };
